308217e9c94ddd110b6bf1b71443de8b6d8978a2
Karasionok <daniilokladnikov@mail.ru>
2025-10-29 20:23:57 +0300

    Обновлен статус задания 111 темы 11. Задание решено Неверно

---
+++ Тема 11/Задания/-Задание 111.py
@@ -0,0 +1,23 @@
+# Решение
+
+k1 = 11
+i1 = 5
+i2 = 4
+k2 = 4
+i3 = 3
+k3 = 1
+I1 = k1 * i1 / 8 + k2 * i2 / 8 + k3 * i3 / 8
+I = 1188 / 36
+total = I - I1
+print(total)
+
+
+
+
+answer = 23
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(11, 111, answer, '1ff1de774005f8da13f42943881c655f'))
