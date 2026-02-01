8a4dd25a6b57a3f12fe891eb8ac78752befdfb46
karasionok <daniilokladnikov@mail.ru>
2026-02-01 23:15:37 +0300

    Обновлен статус задания № 114 Тема: 11 > Верно

---
+++ Тема 11/Задания/+Задание 114.py
@@ -0,0 +1,19 @@
+# Решение
+
+I1 = 480 * 1024 / 2048 / 40 * 8
+print(I1)
+I_code = 6 * 6
+print(I_code)
+
+
+
+
+
+
+answer = 4096
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(11, 114, answer, 'f7efa4f864ae9b88d43527f4b14f750f'))
