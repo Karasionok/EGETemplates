25072e20b232c7fd8264631e93d7dd87018c3198
karasionok <daniilokladnikov@mail.ru>
2026-02-01 22:59:21 +0300

    Обновлен статус задания № 113 Тема: 11 > Неверно

---
+++ Тема 11/Задания/-Задание 113.py
@@ -0,0 +1,16 @@
+# Решение
+
+I1 = 53 * 1024 * 1024 / 282952
+print(2 ** 14)
+
+
+
+
+
+answer = 16385
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(11, 113, answer, 'f43764367fa4b73ba947fae71b0223a4'))
