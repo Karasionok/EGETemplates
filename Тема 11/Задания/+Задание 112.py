5543a993a66a245bd75800c0f286f990ed0d1d7a
karasionok <daniilokladnikov@mail.ru>
2026-02-01 22:56:40 +0300

    Обновлен статус задания № 112 Тема: 11 > Верно

---
+++ Тема 11/Задания/+Задание 112.py
@@ -0,0 +1,17 @@
+# Решение
+
+
+I1 = 11 * 1024 * 1024 * 1024 / 3845627
+i = 3072 * 8 / 2783
+print(257)
+
+
+
+
+answer = 257
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(11, 112, answer, 'd96409bf894217686ba124d7356686c9'))
