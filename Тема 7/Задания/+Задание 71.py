37ac1c98f3638f442377acdcbf13768d6c269334
Karasionok <daniilokladnikov@mail.ru>
2026-03-11 17:19:08 +0300

    Обновлен статус задания № 71 Тема: 7 > Верно

---
+++ Тема 7/Задания/+Задание 71.py
@@ -0,0 +1,16 @@
+# Решение
+
+i = 24
+I1 = 1920 * 1080 * 24 / 8 / 1024
+I1_new = 0.2 * I1
+I = I1_new * 600 / 1024
+print((750 - I) * 1024 / 600)
+
+
+answer = 65
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(7, 71, answer, 'fc490ca45c00b1249bbe3554a4fdf6fb'))
