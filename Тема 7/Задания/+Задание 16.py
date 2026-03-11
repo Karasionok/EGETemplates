400ac30b4e98186741bec06b276088fd7679a04a
Karasionok <daniilokladnikov@mail.ru>
2026-03-11 17:14:15 +0300

    Обновлен статус задания № 16 Тема: 7 > Верно

---
+++ Тема 7/Задания/+Задание 16.py
@@ -0,0 +1,16 @@
+# Решение
+
+
+i = 12
+I1 = 12 * 1024 * 768 / 8 / 1024
+I1_new = 0.8 * I1
+print(I1_new / 200)
+
+
+answer = 5
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(7, 16, answer, 'e4da3b7fbbce2345d7772b0674a318d5'))
