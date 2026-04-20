03145fe59ce3f6678b8670378917ad3891bc4e33
Karasionok <daniilokladnikov@mail.ru>
2026-04-20 17:08:58 +0300

    Обновлен статус задания № 2303 Тема: 23 > Верно

---
+++ Тема 23/Задания/+Задание 2303.py
@@ -0,0 +1,30 @@
+# Решение
+
+def f(b, e):
+    if b == e:
+        return 1
+    if b < e or b == 12 or b == 15:
+        return 0
+    if b % 2 == 0 and b % 3 == 0:
+        return f(b - 1, e) + f(b // 2, e) + f(b // 3, e)
+    elif b % 2 == 0:
+        return f(b - 1, e) + f(b // 2, e)
+    elif b % 3 == 0:
+        return f(b - 1, e) + f(b // 3, e)
+    else:
+        return f(b - 1, e)
+
+
+print(f(19, 1))
+
+
+
+
+
+answer = 43
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(23, 2303, answer, '17e62166fc8586dfa4d1bc0e1742c08b'))
