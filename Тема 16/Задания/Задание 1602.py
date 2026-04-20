97b5bcfb69776f1cc574f11de21932320d381cd1
karasionok <daniilokladnikov@mail.ru>
2026-04-20 17:04:26 +0300

    решение задач

--- Тема 16/Задания/Задание 1602.py
+++ Тема 16/Задания/Задание 1602.py
@@ -1,6 +1,18 @@
 # Решение
 
 
+count = 0
+lst = [0] * 10 ** 9
+for n in range(10 ** 9):
+    if n == 0:
+        lst[0] = 0
+    elif n % 2 != 0:
+        lst[n] = lst[n - 1] + 1
+    elif n > 0 and n % 2 == 0:
+        lst[n] = lst[n // 2]
+    if lst[n] == 2:
+        count += 1
+print(count)
 
 
 
