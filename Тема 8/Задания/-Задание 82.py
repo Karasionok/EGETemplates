6a96c8a10af5f2feb5f8c8ba5f566e377de69606
Karasionok <daniilokladnikov@mail.ru>
2026-04-27 17:58:30 +0300

    Обновлен статус задания № 82 Тема: 8 > Неверно

--- Тема 8/Задания/-Задание 82.py
+++ Тема 8/Задания/-Задание 82.py
@@ -2,15 +2,15 @@
 from itertools import product
 
 lst = list(product(sorted('СТРОКА'), repeat=5))
-for i in range(len(lst) - 1, -1, -1):
-    if i % 2 == 0 and lst[i][0] not in 'АСТ' and lst[i].count('О') == 0:
-        print(i, lst[i])
+for i in range(len(lst), 0, -1):
+    if i % 2 == 0 and lst[i - 1][0] not in 'АСТ' and lst[i - 1].count('О') == 0:
+        print(i, lst[i - 1])
 
 
 
 
 
-answer = 5182
+answer = 5184
 
 #
 
