3e4ffc859ee2e7eb07c23267d7816d035aa21b49
Karasionok <daniilokladnikov@mail.ru>
2026-06-01 17:26:26 +0300

    Обновлен статус задания № 115 Тема: 11 > Верно

---
+++ Тема 11/Задания/+Задание 115.py
@@ -0,0 +1,17 @@
+# Решение
+
+I1 = 1000 / 50
+i = 10 * 5 / 8
+print(I1 - 7)
+
+
+
+
+
+answer = 13
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(11, 115, answer, 'c51ce410c124a10e0db5e4b97fc2af39'))
