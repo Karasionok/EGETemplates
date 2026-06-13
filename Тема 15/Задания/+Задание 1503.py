b89d6f95562fe2636d89799ba6fd46e51244c885
Karasionok <daniilokladnikov@mail.ru>
2026-06-13 21:09:30 +0300

    Обновлен статус задания № 1503 Тема: 15 > Верно

---
+++ Тема 15/Задания/+Задание 1503.py
@@ -0,0 +1,22 @@
+# Решение
+
+
+# for a in range(1000):
+#     f = True
+#     for x in range(1000):
+#         for y in range(1000):
+#             if (((y < 20) <= (x > 70)) or (not ((x < a) <= (y > a)))) == 0:
+#                 f = False
+#
+#     if f:
+#         print(a)
+#
+
+
+answer = 71
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(15, 1503, answer, 'e2c420d928d4bf8ce0ff2ec19b371514'))
