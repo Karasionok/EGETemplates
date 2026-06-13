499f44ba7ef79034f885159ebda9a7f0d9898131
Karasionok <daniilokladnikov@mail.ru>
2026-06-13 19:01:53 +0300

    Обновлен статус задания № 73 Тема: 7 > Верно

---
+++ Тема 7/Задания/+Задание 73.py
@@ -0,0 +1,18 @@
+# Решение
+
+
+I1 = 1536 * 1024 * 12 / 8 / 1024
+I_p = I1 * 150
+I_t = 288 * 240
+print(I_t)
+print(I_t / I_p)
+
+
+
+answer = 80
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(7, 73, answer, 'f033ab37c30201f73f142449d037028d'))
